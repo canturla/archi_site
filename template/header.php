613ec5dc2f080a4ce938ebdf73d8036160332995
Mickaël <mickaelgable20@gmail.com>
2016-11-21 16:35:32 +0100

    Quelques changement sur le css du menu

--- template/header.php
+++ template/header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
   <head>
+    <meta name="name" content="content" charset="utf-8">
     <title>canturla</title>
     <!-- CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css">
     <link rel="stylesheet" href="template/css/header.css" media="screen" title="no title">
-    <link rel="stylesheet" href="template/css/footer.css">
+    <link rel="stylesheet" href="template/css/footer.css" media="screen" title="no title">
     <link rel="stylesheet" href="template/css/menu.css" media="screen" title="no title">
+    <link rel="stylesheet" href="template/css/main.css" media="screen" title="no title">
     <!-- JAVASCRIPT -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
   </head>
@@ -16,5 +18,3 @@
       <img id="logo_header" src="/template/images/logo_header.jpg" alt="logo" />
 
     </header>
-
-
